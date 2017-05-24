d8adc6667e601571d468456f25309c5c315bf88b
Eli Williamson <eliwilliamson@gmail.com>
2017-05-24 18:28:29 -0400

    hook up drinks to cms

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,6 +1,9 @@
 import CMS from "netlify-cms";
 import FoodPreview from "./cms/food-preview";
+import DrinkPreview from "./cms/drink-preview";
+import GalleryPreview from "./cms/gallery-preview";
 
 CMS.registerPreviewStyle("../css/main.css");
 CMS.registerPreviewTemplate("food", FoodPreview);
+CMS.registerPreviewTemplate("drinks", DrinkPreview);
 CMS.registerPreviewTemplate("gallery", GalleryPreview);
--- src/js/cms/drink-preview.js
+++ src/js/cms/drink-preview.js
@@ -5,15 +5,13 @@ function urlize(value) {
   return value.toLowerCase().replace(/\s/g, "-").replace(/-+/, "-");
 }
 
-export default class FoodPreview extends Component {
+export default class DrinkPreview extends Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     const data = entry.get("data") ? entry.get("data").toJS() : {};
 
     return <section className="content" style={{padding: 16}}>
-      <h1>Food</h1>
-      <h2>{ data.Categories.map((category) => <em>{ category }</em>)}</h2>
-      <hr/>
+      <h1>Drinks</h1>
       <h3>{ data.type }</h3>
       <ul>
         <li>
